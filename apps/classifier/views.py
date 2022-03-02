9ce90d8ffd77e11dea3422227c8d20aec75f0dd6
Romeo Encinares <romeoncrs@gmail.com>
2022-03-02 18:54:12 +0800

    Create function to classify images

--- apps/classifier/views.py
+++ apps/classifier/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render
 from .models import imageDog
 from .forms import imageDogForm
+from fastbook import *
+from fastai.vision.all import *
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+BASE_DIR = Path(__file__).resolve().parent.parent
+filepath = os.path.join(BASE_DIR, 'classifier/model.pkl')
+model = load_learner(filepath)
+classes = model.dls.vocab
+
+def classify(img_file):
+    img = PILImage.create(img_file)
+    prediction = model.predict(img)
+    probs_list = prediction[2].numpy()
+    #print(classes[prediction[1].item()])
+    #print({round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)})
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
 
 # Create your views here.
 def index(request):
     form = imageDogForm(request.POST, request.FILES)
-
-    print("test")
+    result = {}
+    #print(result)
 
     if form.is_valid():
         image = request.FILES['image']
-        print(image)
+        result = classify(image)
     
     context = {
         'form' : form,
+        'result' : result,
     }
 
     return render(request, 'index.html', context)
