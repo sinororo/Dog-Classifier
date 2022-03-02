e81fccc1387f0494ff76d2f805125c89c9c1e661
Romeo Encinares <romeoncrs@gmail.com>
2022-03-02 21:07:42 +0800

    Add attrs to image field

--- apps/classifier/forms.py
+++ apps/classifier/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import imageDog
 
 class imageDogForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.ClearableFileInput(), required=True)
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'class': 'form-control', 'type': 'file', 'id': 'formFile'}), required=True)
 
     class Meta:
         model = imageDog
