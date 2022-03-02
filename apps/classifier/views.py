63d152ce2c0e83751be82c09810946eb3f9e9e8b
Romeo Encinares <romeoncrs@gmail.com>
2022-03-02 21:09:52 +0800

    Add way to render image using uri

--- apps/classifier/views.py
+++ apps/classifier/views.py
@@ -3,6 +3,7 @@ from .models import imageDog
 from .forms import imageDogForm
 from fastbook import *
 from fastai.vision.all import *
+from base64 import b64encode
 import pathlib
 
 temp = pathlib.PosixPath
@@ -13,12 +14,15 @@ model = load_learner(filepath)
 classes = model.dls.vocab
 
 def classify(img_file):
-    img = PILImage.create(img_file)
+    img = PILImage.create(img_file[0])
     prediction = model.predict(img)
     probs_list = prediction[2].numpy()
-    #print(classes[prediction[1].item()])
-    #print({round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)})
+    encoded = b64encode(img_file[1])
+    encoded = encoded.decode('ascii')
+    mime = "image/jpg"
+    image_uri = "data:%s;base64,%s" % (mime, encoded)
     return {
+        'image' : image_uri,
         'category': classes[prediction[1].item()],
         'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
     }
@@ -30,7 +34,7 @@ def index(request):
     #print(result)
 
     if form.is_valid():
-        image = request.FILES['image']
+        image = [request.FILES['image'], form.cleaned_data['image'].file.read()]
         result = classify(image)
     
     context = {
