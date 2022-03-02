e7930bdc393d8cb578aa53eb9cd83f1303fe9a01
Romeo Encinares <romeoncrs@gmail.com>
2022-03-02 15:07:12 +0800

    Render form in index template

--- apps/classifier/views.py
+++ apps/classifier/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import imageDog
+from .forms import imageDogForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    form = imageDogForm(request.POST or None)
+
+    if form.is_valid():
+        print("Image Uploaded")
+    
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'index.html', context)
